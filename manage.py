dab3fcf2a8192eb01912e49251fcfbb29096a3fa
Tara Tran <38674048+taratran@users.noreply.github.com>
2019-10-16 17:33:30 -0400

    fixed spelling error

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django.core.management imoprt execute_from_command_line 
+            import django.core.management import execute_from_command_line 
         except ImportError:
             raise ImportError(
                               "Couldn't import Django. Are you sure it's installed and "
